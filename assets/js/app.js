c03a1a60787a8ec3dda8867861133df316aa3370
Ariana Cabana <acabana.0904@gmail.com>
2017-04-18 16:04:21 -0500

    Desafio V: finalizado

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// elementos necesarios
+var contenedor = document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+var verde = document.getElementById("verde");
+var amarillo = document.getElementById("amarillo");
+
+// window.onload = function() {
+contenedor.style.position = "relative";
+contenedor.style.background = "DarkGray";
+contenedor.style.width = "600px";
+contenedor.style.padding = "50px";
+
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
+// }
